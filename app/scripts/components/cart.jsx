11a5672d21d349cd6eb61d7305f3476148a17674
git-directory-deploy <patrickratigan@patricks-mbp.home>
2017-04-09 13:39:13 -0400

    working on username

--- app/scripts/components/cart.jsx
+++ app/scripts/components/cart.jsx
@@ -94,7 +94,7 @@ var CartLayout = React.createClass({
           </div>
         </div>
         <div className="row">
-          <div className="col-md-4"></div>
+          <div className="col-xs-12 col-md-4"></div>
 
           <div className="well col-xs-12 col-md-4 checkoutwellcolor">
             <h3>Total</h3>
